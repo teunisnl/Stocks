c9a0635cc420b583c5a70f83b0849dab38745fb8
teunisnl <betesda@gmail.com>
2017-06-29 18:59:41 +0200

    Introduced most interfaces, implemention comes later

---
+++ Stocks/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stocks
+{
+	public class Game
+	{
+		List<IDice> dices = new List<IDice>();
+		List<IPlayer> players = new List<IPlayer>();
+		List<ICard> cards = new List<ICard>();
+		List<IStockChange> stockchanges = new List<IStockChange>();
+		IBank bank;
+
+		public Game()
+		{
+			InitDices();
+		}
+
+		#region Inits
+		private void InitDices()
+		{
+			dices.Add(new Dice(1, 6));	
+			dices.Add(new Dice(1, 6));
+		}
+		#endregion Inits
+	}
+}
---
+++ Stocks/Interfaces/IBank.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace Stocks
+{
+	public interface IBank
+	{
+	}
+}
